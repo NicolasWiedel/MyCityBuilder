df3ff229b57a75d24320419bdd57389c95cc0252
nicolaswiedel <nicolaswiedel@googlemail.com>
2024-03-25 18:15:32 +0100

    MouseInput

---
+++ MyCityBuilder/Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask mouseInputMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    public void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
+            {
+                Vector3 position = hit.point;
+                Debug.Log(position);
+            }
+        }  
+    }
+}
