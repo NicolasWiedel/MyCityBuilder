995863a5129885c5d66168e7e61cc2c5ca766c8c
nicolaswiedel <nicolaswiedel@googlemail.com>
2024-04-25 17:49:37 +0200

    CameraMovement 1

--- MyCityBuilder/Assets/Scripts/CameraMovement.cs
+++ MyCityBuilder/Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    Vector3? basePointerPosition = null;
+    private float cameraMovementSpeed = 0.05f;
+    private int cameraXMin, cmareaXMax, cameraZMin, cameraZMax;
+
     // Start is called before the first frame update
     void Start()
     {
